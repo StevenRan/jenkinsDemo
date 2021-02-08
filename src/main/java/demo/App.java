64d6d4ed6915983b1ac6fd3f8e8beefc31ced9ab
rankun@hotmail.com <rankun@hotmail.com>
2021-02-08 15:51:16 +0800

    commit2

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class App {
+	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public static int dec(int a, int b) {
+		return a - b;
+	}
+ 
+}
--- src/test/java/demo/AppTest.java
+++ src/test/java/demo/AppTest.java
@@ -1,12 +1,81 @@
 package demo;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class AppTest {
+	
+	private App appi = new App();
 	
 	@Test
 	public void TestMethod1() {
 		System.out.println("testMethod1 executed.");
 	}
+	
+	@Test(groups= {"group1"})
+	public void testAdd() {
+		int expected = 10;
+		Assert.assertEquals(appi.add(3,  7), expected);
+	}
 
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("before suite executed");
+	}
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("after suite executed");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("before test executed");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("after test executed");
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("before class executed");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("after class executed");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("before method executed");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("after method executed");
+	}
+	
+	@BeforeGroups("group1")
+	public void beforeGroup() {
+		System.out.println("before group executed");
+	}
+	
+	@AfterGroups("group1")
+	public void afterGroup() {
+		System.out.println("after group executed");
+	}
 }
